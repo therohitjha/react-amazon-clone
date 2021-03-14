e87654fabce57731320739870ac9386c8d21241c
Rohit Jha <therohitjha@gmail.com>
2021-03-14 23:26:36 +0530

    Updates

---
+++ precache-manifest.a4d752d95d5125f0d9937b3d659aef01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8ee5827cac501d21d9ac784b9d973e",
+    "url": "/react-amazon-clone/index.html"
+  },
+  {
+    "revision": "5a272801ea375d77ef6f",
+    "url": "/react-amazon-clone/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "d0dce50e07be3c198cfe",
+    "url": "/react-amazon-clone/static/js/2.9d7935f7.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/react-amazon-clone/static/js/2.9d7935f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a272801ea375d77ef6f",
+    "url": "/react-amazon-clone/static/js/main.bf1aa44a.chunk.js"
+  },
+  {
+    "revision": "04c0ebc6ad64b9b72b43",
+    "url": "/react-amazon-clone/static/js/runtime-main.8287139a.js"
+  }
+]);
